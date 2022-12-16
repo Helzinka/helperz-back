cb398f25cf07fe1aac52202d6a2559b677b53a81
Helzinka <leveque.fra@gmail.com>
2022-12-16 15:23:24 +0100

    add user model

--- models/users.js
+++ models/users.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose")
 
+const latLong = mongoose.Schema({
+	lat: Number,
+	long: Number,
+})
+
 const userSchema = mongoose.Schema({
 	username: String,
+	lastname: String,
 	password: String,
+	email: String,
+	isHelper: Boolean,
+	available: Boolean,
+	review: Number,
+	location: latLong,
+	skills: [String],
 	token: String,
 })
 
