924279b6b94e7fa1fa8418b173abcf0a84ae2871
Helzinka <leveque.fra@gmail.com>
2022-12-21 09:37:12 +0100

    change result name

--- routes/announces.js
+++ routes/announces.js
@@ -35,7 +35,7 @@ router.post("/", (req, res) => {
 	}).then((data) => {
 		if (data) {
 			addAnnounceToUser(data.userOwner, data._id)
-			res.json({ resutl: true, data: data })
+			res.json({ result: true, data: data })
 		} else {
 			res.json({ result: false, error: "Can't add announces" })
 		}
