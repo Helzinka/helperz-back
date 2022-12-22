355a3ea738450ef3726e395faa1e1ee92cde550d
Helzinka <leveque.fra@gmail.com>
2022-12-22 15:30:55 +0100

    add avatar

--- models/users.js
+++ models/users.js
@@ -1,41 +1,42 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 // sous-document position
 const Position = mongoose.Schema({
-  name: String,
-  lat: Number,
-  long: Number,
-});
+	name: String,
+	lat: Number,
+	long: Number,
+})
 
 // sous-document helperz
 const Helperz = mongoose.Schema({
-  isHelperz: Boolean,
-  available: Boolean,
-  skills: [String],
-  review: Number,
-  location: Position,
-});
+	isHelperz: Boolean,
+	available: Boolean,
+	skills: [String],
+	review: Number,
+	location: Position,
+})
 
 // Model user
 // clef-etrangere vers collection "announces"
 // user a plusieurs annonces
 const userSchema = mongoose.Schema(
-  {
-    username: String,
-    lastname: String,
-    password: String,
-    email: String,
-    // description: String,
-    helperz: Helperz,
-    token: String,
-    announces: [{ type: mongoose.Schema.Types.ObjectId, ref: "announces" }],
-  },
-  {
-    versionKey: false,
-    timestamps: true,
-  }
-);
+	{
+		username: String,
+		lastname: String,
+		password: String,
+		email: String,
+		avatar: String,
+		// description: String,
+		helperz: Helperz,
+		token: String,
+		announces: [{ type: mongoose.Schema.Types.ObjectId, ref: "announces" }],
+	},
+	{
+		versionKey: false,
+		timestamps: true,
+	}
+)
 
-const User = mongoose.model("users", userSchema);
+const User = mongoose.model("users", userSchema)
 
-module.exports = User;
+module.exports = User
